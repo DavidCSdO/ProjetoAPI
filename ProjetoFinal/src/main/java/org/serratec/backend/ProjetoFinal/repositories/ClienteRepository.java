f90b6876a97b462cfce7355bad02bee1a274c886
Raphaela Martins <89480140+raaphaa@users.noreply.github.com>
2021-11-08 14:58:44 -0300

    Create ClienteRepository.java

---
+++ ProjetoFinal/src/main/java/org/serratec/backend/ProjetoFinal/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+package org.serratec.backend.ProjetoFinal.repositories;
+
+import org.serratec.backend.ProjetoFinal.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+}
