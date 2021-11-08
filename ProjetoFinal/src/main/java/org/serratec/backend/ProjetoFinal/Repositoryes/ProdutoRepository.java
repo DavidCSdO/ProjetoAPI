6e9754e362b1bcb218f1779b6bb1491b3ad35e05
Raphaela Martins <89480140+raaphaa@users.noreply.github.com>
2021-11-08 14:51:03 -0300

    Create ProdutoRepository.java

---
+++ ProjetoFinal/src/main/java/org/serratec/backend/ProjetoFinal/Repositoryes/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package org.serratec.backend.ProjetoFinal.Repositoryes;
+
+import org.serratec.backend.ProjetoFinal.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+
+}
