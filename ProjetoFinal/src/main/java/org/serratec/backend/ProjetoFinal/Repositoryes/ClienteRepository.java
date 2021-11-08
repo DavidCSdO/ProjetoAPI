3649578798f20c28c06764f01092ec476a307d74
Raphaela Martins <89480140+raaphaa@users.noreply.github.com>
2021-11-08 14:58:49 -0300

    Delete ClienteRepository.java

--- ProjetoFinal/src/main/java/org/serratec/backend/ProjetoFinal/Repositoryes/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package org.serratec.backend.ProjetoFinal.Repositoryes;
-
-import org.serratec.backend.ProjetoFinal.domain.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-}
