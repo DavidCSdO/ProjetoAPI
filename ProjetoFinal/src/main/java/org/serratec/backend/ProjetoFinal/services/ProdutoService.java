7e637ead98636ed19c3dafef8ac8f9a9e34cdbd9
Raphaela Martins <89480140+raaphaa@users.noreply.github.com>
2021-11-08 14:59:05 -0300

    Create ProdutoService.java

---
+++ ProjetoFinal/src/main/java/org/serratec/backend/ProjetoFinal/services/ProdutoService.java
@@ -0,0 +1,9 @@
+package org.serratec.backend.ProjetoFinal.services;
+
+import org.serratec.backend.ProjetoFinal.domain.Produto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService {
+
+}
