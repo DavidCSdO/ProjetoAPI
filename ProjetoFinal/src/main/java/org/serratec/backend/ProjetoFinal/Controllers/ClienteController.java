8c69e4351b385f27e514910f95e94e593e0b06cd
Raphaela Martins <89480140+raaphaa@users.noreply.github.com>
2021-11-09 14:43:10 -0300

    Update ClienteController.java

--- ProjetoFinal/src/main/java/org/serratec/backend/ProjetoFinal/Controllers/ClienteController.java
+++ ProjetoFinal/src/main/java/org/serratec/backend/ProjetoFinal/Controllers/ClienteController.java
@@ -1,10 +1,8 @@
 package org.serratec.backend.ProjetoFinal.Controllers;
 
-	
-	import java.util.List;
-	import java.util.Optional;
-
-	import javax.management.loading.ClassLoaderRepository;
+import java.util.List;
+import java.util.Optional;
+import javax.management.loading.ClassLoaderRepository;
 
 import org.serratec.backend.ProjetoFinal.domain.Cliente;
 import org.serratec.backend.ProjetoFinal.repositories.ClienteRepository;
@@ -24,58 +22,54 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
-	@RestController
-	@RequestMapping("/clientes")
-	public class ClienteController {
-		
-		@Autowired
-		private ClienteRepository clienteRepository;
-		
-		@GetMapping
-		public List<Cliente> listar(){
-			return clienteRepository.findAll();
-		}
-		
-		@GetMapping("/{id}")
-		public ResponseEntity<Cliente> buscar(@PathVariable Long id){
-			Optional<Cliente> cliente = clienteRepository.findById(id);
-			if (cliente.isPresent()) {
-				return ResponseEntity.ok(cliente.get());
-			}
-			return ResponseEntity.notFound().build();
-		}
-		
-		@PutMapping("/att/{id}")
-		@ApiResponses(value = {
-					@ApiResponse(code = 201, message = "Cliente adicionado"),
-					@ApiResponse(code = 401, message = "Erro de autenticação"),
-					@ApiResponse(code = 403, message = "Cliente adicionado"),
-					@ApiResponse(code = 404, message = "Recurso não encontrado")
-		})
-		public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
-			if (!clienteRepository.existsById(id)){
-				return ResponseEntity.notFound().build();
-		}
-			cliente.setId(id);
-			cliente = clienteRepository.save(cliente);
-			return ResponseEntity.ok(cliente);
-		}
-		
-		@DeleteMapping("/dl/{id}")
-		public ResponseEntity<Void> remover (@PathVariable Long id) {
-			if (!clienteRepository.existsById(id)) {
-				return ResponseEntity.notFound().build();
-			}
-			clienteRepository.deleteById(id);
-			return ResponseEntity.noContent().build();
-		}
-		
-		@PostMapping("/add")
-		@ResponseStatus(code = HttpStatus.CREATED)
-		public Cliente inserir(@RequestBody Cliente cliente) {
-			clienteRepository.saveAndFlush(cliente);
-			return cliente;
-		}
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@GetMapping
+	public List<Cliente> listar() {
+		return clienteRepository.findAll();
 	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long id) {
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
 
+	@PutMapping("/att/{id}")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Cliente adicionado"),
+			@ApiResponse(code = 401, message = "Erro de autenticação"),
+			@ApiResponse(code = 403, message = "Cliente adicionado"),
+			@ApiResponse(code = 404, message = "Recurso não encontrado") })
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+		if (!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		cliente.setId(id);
+		cliente = clienteRepository.save(cliente);
+		return ResponseEntity.ok(cliente);
+	}
+
+	@DeleteMapping("/dl/{id}")
+	public ResponseEntity<Void> remover(@PathVariable Long id) {
+		if (!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		clienteRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PostMapping("/add")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Cliente inserir(@RequestBody Cliente cliente) {
+		clienteRepository.saveAndFlush(cliente);
+		return cliente;
+	}
+}
